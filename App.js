cfbefaa8b85f7f35061d38569ad3fb41ee746aad
Bijoin Krishna bairagi <bijon.bairagi@gmail.com>
2023-04-17 14:37:54 +0600

    .

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import WelcomeScreen from './screens/WelcomeScreen';
+import UserScreen from './screens/UserScreen';
+import {Ionicons} from '@expo/vector-icons'
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator screenOptions={{
+        headerStyle: { backgroundColor: '#3c0a6b'},
+        headerTintColor: 'white',
+        drawerActiveTintColor: '#3c0a6b',
+        drawerActiveBackgroundColor:'#f0e1ff',
+        drawerStyle: { backgroundColor: '#cccccc'},
+      }}>
+        <Drawer.Screen name="Welcome" component={WelcomeScreen} options={{
+          drawerLabel: 'Welcome Screen',
+          drawerIcon: (
+            ({color, size}) => (<Ionicons name="home" color = {color} size={size} />)
+            )
+        }} />
+        <Drawer.Screen name="User" component={UserScreen}
+        options={{
+          drawerLabel: 'User Screen',
+          drawerIcon: (
+            ({color, size}) => (<Ionicons name="person" color = {color} size={size} />)
+            )
+        }}  />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
